d72549e9ae9f1b4f4bbf7a6d3a2d9664fd230ebf
Tunahan Soysal <soysaltunahan48@gmail.com>
2023-01-11 17:34:39 +0300

    dev

---
+++ Controllers/LandingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Granota.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult Landing_reg()
+        {
+            return View();
+        }
+        public IActionResult Landing_nonReg()
+        {
+            return View();
+        }
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+        public IActionResult HomePage_admin()
+        {
+            return View();
+        }
+        public IActionResult Restaurant_menu()
+        {
+            return View();
+        }
+    }
+}
