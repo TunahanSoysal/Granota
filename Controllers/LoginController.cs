95cc8facf29ee33b16076047d6108b6368ef4496
Tunahan Soysal <soysaltunahan48@gmail.com>
2023-01-27 02:50:33 +0300

    devFinal

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Granota.Data;
+using Granota.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Granota.Controllers
 {
     public class LoginController : Controller
     {
+
+        private readonly GranotaContext _context;
+
+        public LoginController(GranotaContext context)
+        {
+            _context = context;
+        }
         public IActionResult Login()
         {
             return View();
@@ -11,6 +21,18 @@ namespace Granota.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Id,Name,Password,Email,Phone,Address")] Buyer buyer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(buyer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Login));
+            }
+            return View(buyer);
         }
         public IActionResult Forget_password()
         {
@@ -20,5 +42,7 @@ namespace Granota.Controllers
         {
             return View();
         }
+
+        
     }
 }
