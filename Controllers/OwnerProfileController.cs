25d2b555e2ac345a8ac2c52336b282c7b06761f6
Tunahan Soysal <soysaltunahan48@gmail.com>
2023-01-27 03:20:59 +0300

    Final

--- Controllers/OwnerProfileController.cs
+++ Controllers/OwnerProfileController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Granota.Data;
+using Granota.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
 
 namespace Granota.Controllers
 {
     public class OwnerProfileController : Controller
     {
+
+        private readonly GranotaContext _context;
+
+        public OwnerProfileController(GranotaContext context)
+        {
+            _context = context;
+        }
         public IActionResult owner_orderHistory()
         {
             return View();
@@ -19,6 +30,18 @@ namespace Granota.Controllers
         public IActionResult owner_management()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> owner_management([Bind("ProductName,ProductDescription,ProductPrice")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(owner_profile));
+            }
+            return View(products);
         }
     }
 }
